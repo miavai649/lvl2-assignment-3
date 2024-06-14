0d2bdf1f56a146a0ddc8f8b7a2682d195cfa977a
miavai649 <mahmudulhaquenoor@gmail.com>
2024-06-15 00:02:18 +0600

    user interface created

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from 'express'
-const app = express()
+import express, { Request, Response } from "express";
+const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 'hello world'
+app.get("/", (req: Request, res: Response) => {
+  const a = "hello world";
 
-  res.send(a)
-})
+  res.send(a);
+});
 
-export default app
+export default app;
