1a505576e5ac340190a31ced65098880242dbc88
miavai649 <mahmudulhaquenoor@gmail.com>
2024-06-15 11:34:50 +0600

    sendResponse utility created for not repeating the res sending code block

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { AuthServices } from "./auth.service";
 import catchAsync from "../utils/catchAsync";
+import sendResponse from "../utils/sendResponse";
 
 const signUp = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthServices.signUp(req.body);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: "User created successfully",
     data: result,
