9fe9f3261e493d92ff2305de79ed93d7238a281b
miavai649 <mahmudulhaquenoor@gmail.com>
2024-06-16 08:32:21 +0600

    service controller created

---
+++ src/app/modules/Service/service.controller.ts
@@ -0,0 +1,17 @@
+import catchAsync from '../utils/catchAsync'
+import sendResponse from '../utils/sendResponse'
+import { ServiceServices } from './Service.service'
+
+const createService = catchAsync(async (req, res) => {
+  const result = await ServiceServices.createServiceIntoDb(req.body)
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Service created successfully',
+    data: result
+  })
+})
+
+export const ServiceControllers = {
+  createService
+}
